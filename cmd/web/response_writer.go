562338b323fc8f6412535ab8405f20cb63f1753e
Felipe David <felipedavid.huh@gmail.com>
2022-09-26 13:58:21 -0300

    Deveria ter ido no commit de requests logging :/

---
+++ cmd/web/response_writer.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+// responseWriter é apenas um wrapper em volta do ResponseWriter entregue pelo pacote http, com o único intuito
+// de tornar disponível o status da resposta após a chamada ao método WriteHeader
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w}
+}
+
+func (rw *responseWriter) WriteHeader(status int) {
+	rw.status = status
+	rw.ResponseWriter.WriteHeader(status)
+}
