bd3d4d0179f35a94d04a78c5ad5509071a05648a
Felipe David <felipedavid.huh@gmail.com>
2022-10-26 16:41:50 -0300

    Simplificando handlers para se livrar de alguns code smells

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,14 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
 func (a *app) routes() http.Handler {
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", a.home)
-	mux.HandleFunc("/games", a.games)
-	mux.HandleFunc("/login", a.login)
+	router := httprouter.New()
 
-	return a.logRequest(mux)
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+
+	router.HandlerFunc(http.MethodGet, "/", a.homePage)
+	router.HandlerFunc(http.MethodGet, "/games", a.viewGame)
+	router.HandlerFunc(http.MethodPost, "/games", a.createGame)
+	router.HandlerFunc(http.MethodDelete, "/games", a.deleteGame)
+	router.HandlerFunc(http.MethodGet, "/login", a.loginPage)
+
+	return a.logRequest(router)
 }
