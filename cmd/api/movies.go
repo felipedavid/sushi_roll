03f12d505bb22a338ccd2948f35d9db206b96794
Felipe David <felipedavid.huh@gmail.com>
2023-03-23 10:03:52 -0300

    Allowing new syntax to define the movie's runtime

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -40,10 +40,10 @@ func (app *application) moviesHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	case http.MethodPost:
 		var input struct {
-			Title   string   `json:"title"`
-			Year    int32    `json:"year"`
-			Runtime int32    `json:"runtime"`
-			Genres  []string `json:"genres"`
+			Title   string       `json:"title"`
+			Year    int32        `json:"year"`
+			Runtime data.Runtime `json:"runtime"`
+			Genres  []string     `json:"genres"`
 		}
 
 		err := app.readJSON(w, r, &input)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,10 +1,39 @@
 package data
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
-func (r Runtime) MarshalJSON() ([]byte, error) {
+func (r *Runtime) MarshalJSON() ([]byte, error) {
 	res := fmt.Sprintf(`"%d mins"`, r)
 	return []byte(res), nil
+}
+
+func (r *Runtime) UnmarshalJSON(js []byte) error {
+	json, err := strconv.Unquote(string(js))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(json, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	runtime, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(runtime)
+
+	return nil
 }
