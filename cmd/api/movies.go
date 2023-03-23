017ccbfca525489df5088b06988b90dc59c135f6
Felipe David <felipedavid.huh@gmail.com>
2023-03-23 12:56:32 -0300

    Adding some input validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/felipedavid/sushi_roll/internal/data"
+	"github.com/felipedavid/sushi_roll/internal/validator"
 	"net/http"
 	"strconv"
 	"strings"
@@ -49,6 +50,28 @@ func (app *application) moviesHandler(w http.ResponseWriter, r *http.Request) {
 		err := app.readJSON(w, r, &input)
 		if err != nil {
 			app.badRequestResponse(w, r, err)
+			return
+		}
+
+		v := validator.New()
+
+		v.Check(input.Title != "", "title", "must be provided")
+		v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+		v.Check(input.Year != 0, "year", "must be provided")
+		v.Check(input.Year <= 1888, "year", "must be greater than 1888")
+		v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+		v.Check(input.Runtime != 0, "runtime", "must be provided")
+		v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+
+		v.Check(input.Genres != nil, "genres", "must be provided")
+		v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+		v.Check(len(input.Genres) <= 5, "genres", "must not contains more than 5 genres")
+		v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate genres")
+
+		if !v.Valid() {
+			app.failedValidationResponse(w, r, v.Errors)
 			return
 		}
 
